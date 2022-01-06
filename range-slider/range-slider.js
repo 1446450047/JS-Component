72c98deb9c07512c94b7047d3a672ee8c12683a0
1446450047 <1446450047@qq.com>
2022-01-06 21:19:06 +0800

    完成range-slider

---
+++ range-slider/range-slider.js
@@ -0,0 +1,3 @@
+document.querySelector(".ranger-slider").addEventListener("input" ,function (){
+    document.querySelector(".ranger-slider-content").innerHTML = document.querySelector(".ranger-slider").value
+})
