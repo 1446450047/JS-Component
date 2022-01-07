dec88ebc14f681a7bad4089af45b6391463da954
1446450047 <1446450047@qq.com>
2022-01-07 20:19:48 +0800

    调整label错误

--- range-slider/range-slider.js
+++ range-slider/range-slider.js
@@ -1,3 +1,3 @@
-document.querySelector(".ranger-slider").addEventListener("input" ,function (){
-    document.querySelector(".ranger-slider-content").innerHTML = document.querySelector(".ranger-slider").value
+document.querySelector(".ranger-slider input").addEventListener("input" ,function (){
+    document.querySelector(".ranger-slider-content").innerHTML = document.querySelector(".ranger-slider input").value
 })
