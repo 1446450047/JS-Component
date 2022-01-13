259250a3ad90927dc73bc571eccaf70b35414d34
1446450047 <1446450047@qq.com>
2022-01-13 20:07:38 +0800

    添加scroll indicator简单样式

--- to-top/to-top.js
+++ to-top/to-top.js
@@ -2,9 +2,7 @@ window.onscroll = function (){
     if(document.documentElement.scrollTop > 200){
         document.querySelector(".to-top").style.display = "block"
     }
-
     if(document.documentElement.scrollTop < 200){
         document.querySelector(".to-top").style.display = "none"
     }
-
 }
