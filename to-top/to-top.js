3b08658df819e5b7196afac058d464da89eac7bc
1446450047 <1446450047@qq.com>
2022-01-13 20:11:51 +0800

    添加to-top回到顶部的js

--- to-top/to-top.js
+++ to-top/to-top.js
@@ -1,8 +1,12 @@
+const toTop  = document.querySelector(".to-top")
 window.onscroll = function (){
     if(document.documentElement.scrollTop > 200){
-        document.querySelector(".to-top").style.display = "block"
+        toTop.style.display = "block"
     }
     if(document.documentElement.scrollTop < 200){
-        document.querySelector(".to-top").style.display = "none"
+        toTop.style.display = "none"
     }
 }
+toTop.addEventListener("click",function (){
+    document.documentElement.scrollTop = 0;
+})
