dc87eb08dc6f612af050b0a1c30c344fd7697b4c
1446450047 <1446450047@qq.com>
2022-01-18 22:06:05 +0800

    完成tabs的js部分

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -2,5 +2,10 @@ let buttons = document.querySelectorAll(".tabs-header button")
 let contents = document.querySelectorAll(".tabContent")
 
 for(let i = 0; i < buttons.length; i++){
-
+    buttons[i].addEventListener("click",function (){
+        for(let j = 0; j < contents.length; j++){
+            j === i ? contents[j].style.display = "block" : contents[j].style.display = "none";
+            j === i ? buttons[j].classList.add("active") : buttons[j].classList.remove("active");
+        }
+    })
 }
