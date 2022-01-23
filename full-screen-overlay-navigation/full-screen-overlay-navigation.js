0352c10a981c8e57b9ff861f8bee35951468b334
1446450047 <1446450047@qq.com>
2022-01-23 15:31:22 +0800

    full screen overlay navigation 修改隐藏方式为width = 0通过切换class来控制

--- full-screen-overlay-navigation/full-screen-overlay-navigation.js
+++ full-screen-overlay-navigation/full-screen-overlay-navigation.js
@@ -1,8 +1,8 @@
 const fullScreenOverlayNavigationWrapper = document.querySelector(".full-navigation-wrapper")
 document.querySelector(".full-navigation-wrapper-button").addEventListener("click",function (){
-    fullScreenOverlayNavigationWrapper.style.display = "block"
+    fullScreenOverlayNavigationWrapper.classList.toggle("hidden")
 })
 document.querySelector(".full-navigation-wrapper .close").addEventListener("click",function (){
-    fullScreenOverlayNavigationWrapper.style.display = "none"
+    fullScreenOverlayNavigationWrapper.classList.toggle("hidden")
 })
 
