e185aabd35f6bd72719edce75173add55a3d47a3
1446450047 <1446450047@qq.com>
2022-01-26 14:45:38 +0800

    accordion 添加js

---
+++ accordion/accordion.js
@@ -0,0 +1,9 @@
+const accordions = document.querySelectorAll(".accordion");
+const panels =  document.querySelectorAll(".panel")
+console.log(panels[0].scrollHeight);
+console.log(panels[0].style.height);
+for(let i = 0; i < accordions.length; i++){
+    accordions[i].addEventListener("click",function (){
+        panels[i].style.height = panels[i].style.height === "" ?  panels[i].scrollHeight  + "px" : ""
+    })
+}
