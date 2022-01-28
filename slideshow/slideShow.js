e499701b31c1098a2a207b7625a63a3afffa384d
1446450047 <1446450047@qq.com>
2022-01-28 12:53:07 +0800

    添加previous和next点击事件

--- slideshow/slideShow.js
+++ slideshow/slideShow.js
@@ -7,3 +7,21 @@ const automaticSlideShow = function(){
     slideShows[imageIndex].classList.toggle("active")
 }
 setInterval(automaticSlideShow,4000)
+
+const previous = document.querySelector(".previous")
+const next = document.querySelector(".next")
+
+next.addEventListener("click",function(){
+    slideShows[imageIndex].classList.toggle("active")
+    imageIndex++
+    imageIndex = imageIndex >= 4 ? 0: imageIndex;
+    slideShows[imageIndex].classList.toggle("active")
+})
+
+previous.addEventListener("click",function (){
+    slideShows[imageIndex].classList.toggle("active")
+    imageIndex--
+    imageIndex = imageIndex < 0 ? 3: imageIndex;
+    slideShows[imageIndex].classList.toggle("active")
+
+})
