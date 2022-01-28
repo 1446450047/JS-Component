e9a450ef16ff929f8dc6795135fd996b0c3c6d14
1446450047 <1446450047@qq.com>
2022-01-28 13:15:10 +0800

    完成slideshow的js部分

--- slideshow/slideShow.js
+++ slideshow/slideShow.js
@@ -1,27 +1,46 @@
 let imageIndex = 0;
-const slideShows = document.querySelectorAll(".slideshow")
-const automaticSlideShow = function(){
-    slideShows[imageIndex].classList.toggle("active")
+const slideShows = document.querySelectorAll(".slideshow");
+const dots = document.querySelectorAll(".dot");
+const automaticSlideShow = function () {
+    slideShows[imageIndex].classList.toggle("active");
+    dots[imageIndex].classList.toggle("active");
     imageIndex++;
-    imageIndex = imageIndex >= 4 ? 0: imageIndex;
-    slideShows[imageIndex].classList.toggle("active")
-}
+    imageIndex = imageIndex >= 4 ? 0 : imageIndex;
+    dots[imageIndex].classList.toggle("active");
+    slideShows[imageIndex].classList.toggle("active");
+};
 setInterval(automaticSlideShow,4000)
 
-const previous = document.querySelector(".previous")
-const next = document.querySelector(".next")
+const previous = document.querySelector(".previous");
+const next = document.querySelector(".next");
 
-next.addEventListener("click",function(){
-    slideShows[imageIndex].classList.toggle("active")
-    imageIndex++
-    imageIndex = imageIndex >= 4 ? 0: imageIndex;
-    slideShows[imageIndex].classList.toggle("active")
-})
+next.addEventListener("click", function () {
+    slideShows[imageIndex].classList.toggle("active");
+    dots[imageIndex].classList.toggle("active");
+    imageIndex++;
+    imageIndex = imageIndex >= 4 ? 0 : imageIndex;
+    slideShows[imageIndex].classList.toggle("active");
+    dots[imageIndex].classList.toggle("active");
+});
 
-previous.addEventListener("click",function (){
-    slideShows[imageIndex].classList.toggle("active")
-    imageIndex--
-    imageIndex = imageIndex < 0 ? 3: imageIndex;
-    slideShows[imageIndex].classList.toggle("active")
+previous.addEventListener("click", function () {
+    slideShows[imageIndex].classList.toggle("active");
+    dots[imageIndex].classList.toggle("active");
+    imageIndex--;
+    imageIndex = imageIndex < 0 ? 3 : imageIndex;
+    slideShows[imageIndex].classList.toggle("active");
+    dots[imageIndex].classList.toggle("active");
+});
 
-})
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", function () {
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].classList.remove("active");
+            slideShows[i].classList.remove("active");
+
+        }
+        imageIndex = i;
+        dots[imageIndex].classList.toggle("active");
+        slideShows[imageIndex].classList.toggle("active");
+    });
+}
